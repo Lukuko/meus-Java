3c0c62904b2d00c089c13314d8c79c50d5d52f79
Lukuko <77814500+Lukuko@users.noreply.github.com>
2022-05-19 19:59:22 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Isto é um comentário
+        System.out.print("quanto é 3+3? ");
+        // Isto é uma String
+        System.out.print(3 + 3);
+        // Isto é uma soma de números
+    }
+}
