e19b1e5caef6605e25dada87ff7fa6d64e98f138
Lukuko <77814500+Lukuko@users.noreply.github.com>
2022-05-19 20:09:40 -0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,16 @@
+public class Main2 {
+    public static void main(String[] args) {
+        /*
+         * Diferente do python em que se declara o tipo da variável depois do sinal de
+         * igual, o java
+         * é antes do nome da variável
+         */
+        Integer myNum = 20;
+        String name = "João";
+        System.out.println("Meu nome é ");
+        System.out.print(name);
+        System.out.println(", Minha idade é ");
+        System.out.print(myNum);
+        System.out.print(" anos");
+    }
+}
