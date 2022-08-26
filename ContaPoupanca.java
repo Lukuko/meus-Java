a3fb5a2d423897dda9d0333211f118cb30cc1929
Davi menezes <77814500+Lukuko@users.noreply.github.com>
2022-08-26 20:43:43 -0300

    Add files via upload

---
+++ ContaPoupanca.java
@@ -0,0 +1,15 @@
+package br.estacio.banco;
+
+public class ContaPoupanca {
+	private double rendimentoMensal;
+	
+	public double getRendimentoMensal() {
+		return rendimentoMensal;
+	}
+	
+	public void setRendimentoMensal(double rendimentoMensal) {
+		this.rendimentoMensal = rendimentoMensal;
+	}
+
+
+}
